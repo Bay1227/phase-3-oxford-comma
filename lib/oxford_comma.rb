d1d4b82064731ac7c2194efdbf04c0d1a26801ad
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-09-02 20:38:53 -0700

    completed Oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if array.count == 1 
+    array.join
+  elsif array.count == 2 
+    array.join(" and ")
+  else 
+  #   final = array.pop 
+  #   array.join(", ") + ", and" << final
+  
+  # end 
+
+  array.each.collect do |value|
+    if value == array[-1]
+        array[-1] = "and " << value
+    end
+end
+array.join(", ")
+end
 
 end
